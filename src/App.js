639ff9c758f5e8c63dc4820f7e4f677ec54d9ec4
evolshenanigans <gjgutierrez91@gmail.com>
2022-12-12 19:47:01 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import qr from "../src/images/image-qr-code.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="divbox">
+        <img src={qr} className="qrcode" alt="qrcode" />
+        <h1>Improve your front-end skills by building projects </h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Scan the QR code to visit Frontend Mentor and take your coding skills
+          to the next level
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
